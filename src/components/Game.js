72d053b848d75ebafc8fcac2ed6f2c2c6dfac093
magga98 <margretdis@hotmail.com>
2023-04-22 20:19:56 +0000

    heim link

--- src/components/Game.js
+++ src/components/Game.js
@@ -144,7 +144,6 @@ export default function Game() {
           display: "inline-block",
           width: "400px",
           height: "400px",
-          
           borderRadius: 5,
           padding: 5, 
         }}>
